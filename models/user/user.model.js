581469fb88f57e8eba8acf0bfa86881532e82635
pratikwadke02 <82306536+pratikwadke02@users.noreply.github.com>
2022-10-08 01:06:39 +0530

    user model changes

--- models/user/user.model.js
+++ models/user/user.model.js
@@ -5,6 +5,12 @@ module.exports = (sequelize, Sequelize) => {
         primaryKey: true,
         autoIncrement: true,
       },
+      firstName: {
+        type: Sequelize.STRING,
+      },
+      lastName: {
+        type: Sequelize.STRING,
+      },
       email: {
         type: Sequelize.STRING,
       },
