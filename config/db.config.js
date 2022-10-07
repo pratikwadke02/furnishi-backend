b5804274003dfcf69cd97b2f802401ee03926d09
pratikwadke02 <82306536+pratikwadke02@users.noreply.github.com>
2022-10-07 20:39:12 +0530

    env changes

--- config/db.config.js
+++ config/db.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    host: 'localhost',
-    user: 'root',
-    password: 'pratik02',
-    database: 'furnishi',
-    dialect: 'mysql',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    dialect: "mysql",
     pool: {
         max: 5,
         min: 0,
